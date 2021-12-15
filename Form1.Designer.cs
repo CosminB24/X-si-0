a7c8321b4d964b0971b428863b89b8b4ec9a43d6
CosminB24 <96211653+CosminB24@users.noreply.github.com>
2021-12-16 01:25:48 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,372 @@
+﻿
+namespace Proiect_PCLP
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optiuniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jocNouToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iesireToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.instructiuniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.C3 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.egaluri = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.o_castiga = new System.Windows.Forms.Label();
+            this.x_castiga = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optiuniToolStripMenuItem,
+            this.instructiuniToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(952, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // optiuniToolStripMenuItem
+            // 
+            this.optiuniToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jocNouToolStripMenuItem,
+            this.iesireToolStripMenuItem});
+            this.optiuniToolStripMenuItem.Name = "optiuniToolStripMenuItem";
+            this.optiuniToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.optiuniToolStripMenuItem.Text = "Optiuni";
+            // 
+            // jocNouToolStripMenuItem
+            // 
+            this.jocNouToolStripMenuItem.Name = "jocNouToolStripMenuItem";
+            this.jocNouToolStripMenuItem.Size = new System.Drawing.Size(142, 26);
+            this.jocNouToolStripMenuItem.Text = "Joc nou";
+            this.jocNouToolStripMenuItem.Click += new System.EventHandler(this.jocNouToolStripMenuItem_Click);
+            // 
+            // iesireToolStripMenuItem
+            // 
+            this.iesireToolStripMenuItem.Name = "iesireToolStripMenuItem";
+            this.iesireToolStripMenuItem.Size = new System.Drawing.Size(142, 26);
+            this.iesireToolStripMenuItem.Text = "Iesire";
+            this.iesireToolStripMenuItem.Click += new System.EventHandler(this.iesireToolStripMenuItem_Click);
+            // 
+            // instructiuniToolStripMenuItem
+            // 
+            this.instructiuniToolStripMenuItem.Name = "instructiuniToolStripMenuItem";
+            this.instructiuniToolStripMenuItem.Size = new System.Drawing.Size(95, 24);
+            this.instructiuniToolStripMenuItem.Text = "Instructiuni";
+            this.instructiuniToolStripMenuItem.Click += new System.EventHandler(this.instructiuniToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.C3);
+            this.panel1.Controls.Add(this.C2);
+            this.panel1.Controls.Add(this.C1);
+            this.panel1.Controls.Add(this.B3);
+            this.panel1.Controls.Add(this.B2);
+            this.panel1.Controls.Add(this.B1);
+            this.panel1.Controls.Add(this.A3);
+            this.panel1.Controls.Add(this.A2);
+            this.panel1.Controls.Add(this.A1);
+            this.panel1.Location = new System.Drawing.Point(14, 36);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(589, 495);
+            this.panel1.TabIndex = 1;
+            // 
+            // C3
+            // 
+            this.C3.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.C3.Location = new System.Drawing.Point(389, 356);
+            this.C3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(158, 131);
+            this.C3.TabIndex = 8;
+            this.C3.Tag = "8";
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.button_click);
+            // 
+            // C2
+            // 
+            this.C2.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.C2.Location = new System.Drawing.Point(207, 356);
+            this.C2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(158, 131);
+            this.C2.TabIndex = 7;
+            this.C2.Tag = "7";
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.button_click);
+            // 
+            // C1
+            // 
+            this.C1.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.C1.Location = new System.Drawing.Point(22, 356);
+            this.C1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(158, 131);
+            this.C1.TabIndex = 6;
+            this.C1.Tag = "6";
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.button_click);
+            // 
+            // B3
+            // 
+            this.B3.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.B3.Location = new System.Drawing.Point(389, 188);
+            this.B3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(158, 131);
+            this.B3.TabIndex = 5;
+            this.B3.Tag = "5";
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.button_click);
+            // 
+            // B2
+            // 
+            this.B2.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.B2.Location = new System.Drawing.Point(207, 188);
+            this.B2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(158, 131);
+            this.B2.TabIndex = 4;
+            this.B2.Tag = "4";
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.button_click);
+            // 
+            // B1
+            // 
+            this.B1.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.B1.Location = new System.Drawing.Point(22, 188);
+            this.B1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(158, 131);
+            this.B1.TabIndex = 3;
+            this.B1.Tag = "3";
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.button_click);
+            // 
+            // A3
+            // 
+            this.A3.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.A3.Location = new System.Drawing.Point(389, 17);
+            this.A3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(158, 131);
+            this.A3.TabIndex = 2;
+            this.A3.Tag = "2";
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.button_click);
+            // 
+            // A2
+            // 
+            this.A2.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.A2.Location = new System.Drawing.Point(207, 17);
+            this.A2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(158, 131);
+            this.A2.TabIndex = 1;
+            this.A2.Tag = "1";
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.button_click);
+            // 
+            // A1
+            // 
+            this.A1.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.A1.Location = new System.Drawing.Point(22, 17);
+            this.A1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(158, 131);
+            this.A1.TabIndex = 0;
+            this.A1.Tag = "0";
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.button_click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.egaluri);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.o_castiga);
+            this.panel2.Controls.Add(this.x_castiga);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(600, 36);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(358, 263);
+            this.panel2.TabIndex = 2;
+            // 
+            // egaluri
+            // 
+            this.egaluri.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.egaluri.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.egaluri.Location = new System.Drawing.Point(129, 199);
+            this.egaluri.Name = "egaluri";
+            this.egaluri.Size = new System.Drawing.Size(96, 52);
+            this.egaluri.TabIndex = 9;
+            this.egaluri.Text = "0";
+            this.egaluri.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(23, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(313, 48);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Meciuri încheiate la egalitate";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // o_castiga
+            // 
+            this.o_castiga.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.o_castiga.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.o_castiga.Location = new System.Drawing.Point(240, 96);
+            this.o_castiga.Name = "o_castiga";
+            this.o_castiga.Size = new System.Drawing.Size(96, 52);
+            this.o_castiga.TabIndex = 7;
+            this.o_castiga.Text = "0";
+            this.o_castiga.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // x_castiga
+            // 
+            this.x_castiga.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.x_castiga.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.x_castiga.Location = new System.Drawing.Point(240, 47);
+            this.x_castiga.Name = "x_castiga";
+            this.x_castiga.Size = new System.Drawing.Size(96, 52);
+            this.x_castiga.TabIndex = 6;
+            this.x_castiga.Text = "0";
+            this.x_castiga.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(23, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 52);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Jucătorul \"0\"";
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(23, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 52);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Jucătorul \"X\"";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Location = new System.Drawing.Point(600, 296);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(358, 235);
+            this.panel3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 548);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "X si 0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optiuniToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jocNouToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iesireToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem instructiuniToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label egaluri;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label o_castiga;
+        private System.Windows.Forms.Label x_castiga;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
